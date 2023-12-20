244a060805b71de9b81c1b2f183cbeabce614b3a
Ronald Romario Gutierrez Arratia <113565299+Ronald-Gutierrez@users.noreply.github.com>
2023-12-20 10:15:19 -0500

    Logica de movimiento

--- prueba.js
+++ prueba.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const maze = document.getElementById("maze");
-    let playerSquare = document.createElement("div");
-    playerSquare.classList.add("player");
+    const player = document.createElement("div");
+    player.classList.add("player");
+
+    // maze.appendChild(player);
 
     const mazeArray = [
         ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
@@ -13,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         ["X", "", "", "", "X", "", "", "", "", "X",],
         ["X", "", "", "", "", "", "X", "", "M", "X",],
         ["X", "X", "X", "X", "X","X", "X", "X", "X","X"]
+        
     ];
 
     const cells = [];
@@ -35,57 +38,49 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Initialize player in the top-left corner
     let playerPosition = mazeArray[0].length + 1;
+    cells[playerPosition].classList.add("player");
 
-    // Función para mover el cuadrado azul basado en la emoción reconocida
-    function movePlayerSquare(emotion) {
-        const row = Math.floor(playerPosition / mazeArray[0].length);
-        const col = playerPosition % mazeArray[0].length;
-
-        // Elimina el rastro del jugador antes de moverlo
-        cells[playerPosition].classList.remove("player");
-
-        switch(emotion) {
-            case "Happy": // Mueve hacia abajo
-                if (row + 1 < mazeArray.length && mazeArray[row + 1][col] !== "X") {
-                    playerPosition += mazeArray[0].length;
-                }
+    document.addEventListener("keydown", function(e) {
+        switch(e.key) {
+            case "ArrowUp":
+                movePlayer(-mazeArray[0].length);
                 break;
-            case "Sad": // Mueve hacia arriba
-                if (row - 1 >= 0 && mazeArray[row - 1][col] !== "X") {
-                    playerPosition -= mazeArray[0].length;
-                }
+            case "ArrowDown":
+                movePlayer(mazeArray[0].length);
                 break;
-            case "Surprise": // Mueve hacia la derecha
-                if (col + 1 < mazeArray[0].length && mazeArray[row][col + 1] !== "X") {
-                    playerPosition += 1;
-                }
+            case "ArrowLeft":
+                movePlayer(-1);
                 break;
-            case "Angry": // Mueve hacia la izquierda
-                if (col - 1 >= 0 && mazeArray[row][col - 1] !== "X") {
-                    playerPosition -= 1;
-                }
+            case "ArrowRight":
+                movePlayer(1);
                 break;
-            // Maneja otras emociones según sea necesario
         }
-        
+    });
 
-        // Mueve el jugador y actualiza la representación visual en el tablero
-        playerSquare = cells[playerPosition];
-        playerSquare.classList.add("player");
+    function movePlayer(direction) {
+        const newPosition = playerPosition + direction;
+
+        // Check if the new position is within bounds
+        if (newPosition >= 0 && newPosition < cells.length) {
+            // Check for walls or other obstacles (you can customize this part)
+            if (!cells[newPosition].classList.contains("obstacle")) {
+                // Move the player
+                cells[playerPosition].classList.remove("player");
+                playerPosition = newPosition;
+                cells[playerPosition].classList.add("player");
+            }
+        }
+
         // Check if the player reached the goal (customize the goal position as needed)
         if (playerPosition === cells.length - mazeArray[0].length-2) {
             alert("¡Felicidades! Has alcanzado la meta.");
             resetGame();
         }
     }
+
     function resetGame() {
         cells[playerPosition].classList.remove("player");
         playerPosition = mazeArray[0].length + 1;
         cells[playerPosition].classList.add("player");
     }
-    // Actualiza la posición del cuadrado azul cuando se reconoce una nueva emoción
-    window.addEventListener(CY.modules().FACE_EMOTION.eventName, (evt) => {
-        const emotionEnIngles = evt.detail.output.dominantEmotion;
-        movePlayerSquare(emotionEnIngles);
-    });
 });
