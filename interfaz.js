6e1051f8d402e2e7654a8bfc8f4dea39ecfcbaa2
Ronald Romario Gutierrez Arratia <113565299+Ronald-Gutierrez@users.noreply.github.com>
2023-12-20 23:00:35 -0500

    Add Interface

---
+++ interfaz.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtén todos los botones
+    var buttons = document.querySelectorAll('.playButton');
+  
+    // Agrega un evento de clic a cada botón
+    buttons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        // Redirige al usuario a index.html
+        window.location.href = 'index.html';
+      });
+    });
+  });
+  
